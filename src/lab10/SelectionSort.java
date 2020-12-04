0bfdd5ed49ca5e61ccb6577728a9ab380bcde630
jdudley <jsdudley@umich.edu>
2020-12-04 01:28:38 -0500

    corrected spacing issue

--- src/lab10/SelectionSort.java
+++ src/lab10/SelectionSort.java
@@ -6,17 +6,17 @@ public class SelectionSort {
 	/** Creates a new instance of SelectionSort */
     public SelectionSort() {
     }
-    
+
     /* A simple SelectionSort algorithm
-     * pre-condition: 
+     * pre-condition:
      * post-condition:
      * inputs:
      * outputs:
      * special conditions:
-     */    
+     */
     public int[] basicSelectionSort(int[] x) {
         for (int i = 0; i < x.length; ++i) {
-            for (int j= x.length - 1; j > i; --j) {
+            for (int j= x.length-1; j > i; --j) {
                 if (x[i] > x[j]) {
                     temp = x[i];
                     x[i] = x[j];
